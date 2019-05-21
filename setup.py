6b27485e20c3268056fc70f31adcc7b95bf032de
Mircea Ulinic <mulinic@digitalocean.com>
2019-05-21 13:25:16 +0100

    Release 2019.5.1b4

--- setup.py
+++ setup.py
@@ -16,7 +16,7 @@ with open("requirements.txt", "r") as fs:
 
 setup(
     name='salt-sproxy',
-    version='2019.5.0b4',
+    version='2019.5.1b4',
     namespace_packages=['salt_sproxy'],
     packages=find_packages(),
     author='Mircea Ulinic',
