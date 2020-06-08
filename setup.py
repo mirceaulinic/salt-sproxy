c021a12e1b1bdd7894174c0b3def584d1208cfc5
Mircea Ulinic <mirceaulinic@users.noreply.github.com>
2020-06-08 13:16:08 +0100

    Version 2020.6.0

--- setup.py
+++ setup.py
@@ -20,7 +20,7 @@ repo_url = 'https://github.com/{}'.format(repo_slug)
 
 setup(
     name=name,
-    version='2020.5.0',
+    version='2020.6.0',
     namespace_packages=['salt_sproxy'],
     packages=find_packages(),
     author='Mircea Ulinic',
