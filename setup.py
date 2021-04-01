c9130156380c7c1bf801dee2b2255aab124ea252
Mircea Ulinic <mirceaulinic@users.noreply.github.com>
2021-04-01 14:39:44 +0100

    Add salt-extension keyword
    This way, the project will be listed under https://extensions.saltproject.io/

--- setup.py
+++ setup.py
@@ -58,7 +58,7 @@ setup(
     ],
     url='https://github.com/mirceaulinic/salt-sproxy',
     license="Apache License 2.0",
-    keywords=('salt', 'network', 'automation', 'cli', 'proxy', 'minion'),
+    keywords=('salt', 'network', 'automation', 'cli', 'proxy', 'minion', 'salt-extension'),
     project_urls={
         'CI: GitHub Actions': '{}/actions'.format(repo_url),
         'Docs: RTD': 'https://salt-sproxy.readthedocs.io/',
