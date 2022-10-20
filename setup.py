da4a9a15c5b603d5f42423ed8cee88dee173ad91
Mircea Ulinic <mirceaulinic@users.noreply.github.com>
2022-10-20 11:30:56 +0100

    Bump version to 2022.10.0

--- setup.py
+++ setup.py
@@ -20,7 +20,7 @@ repo_url = 'https://github.com/{}'.format(repo_slug)
 
 setup(
     name=name,
-    version='2021.6.1',
+    version='2022.10.0',
     namespace_packages=['salt_sproxy'],
     packages=find_packages(),
     author='Mircea Ulinic',
